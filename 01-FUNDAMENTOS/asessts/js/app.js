ee2b81e042b7a7de13f66557555dde7e7f997ca7
mmelvargash <melannyvargashernandez@gmail.com>
2024-12-27 23:31:39 -0600

    Sección2: introducción a JS y la consola + cod fuente

---
+++ 01-FUNDAMENTOS/asessts/js/app.js
@@ -0,0 +1,29 @@
+var miNombre='mela';
+/*
+var permite poder reemplazar propiedades y objetos propios del objeto global window
+
+*/
+// console.log('hola');
+// console.warn('cuidado no te apresuras');
+// console.error('máquina ....')
+// console.info('es informativo');
+
+// //para imprimir la variable y el valor: como objeto
+// let name= 'Melanny',
+//     lastName1= 'Vargas',
+//     lastName2= 'Hernandez';
+// let fullName= name+lastName1+lastName2;
+// console.log('%c Ojo acá', 'color:blue; font-weight:bold');
+// console.log ({name});
+
+// /*console.table recibe un arreglo que se identifican en llaves
+// cuadradas o como objeto en corchetes
+// */
+// console.table({name, lastName1});
+
+/* 
+funciones del objeto window
+alert
+prompt
+confirm -- bool
+*/
